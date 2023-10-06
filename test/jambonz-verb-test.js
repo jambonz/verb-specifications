94cdfbf8f6c74c02de4a5ab2901a4c44305e162b
Quan HL <quan.luuhoang8@gmail.com>
2023-10-06 13:03:44 +0700

    to use Hook

--- test/jambonz-verb-test.js
+++ test/jambonz-verb-test.js
@@ -75,7 +75,7 @@ test("validate correct verbs", async (t) => {
     },
     {
       "verb": "config",
-      "sipIndialogActionHook": "https://jambonz.or/sipIndialogActionHook"
+      "sipIndialogHook": "https://jambonz.or/sipIndialogActionHook"
     },
     {
       "verb": "config",
