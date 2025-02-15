3700ba66074d6e7685b3a77efaadd9736c5c8705
Hoan Luu Huu <110280845+xquanluu@users.noreply.github.com>
2025-02-15 21:10:11 +0700

    deepgram options noDelay (#86)

--- test/jambonz-verb-test.js
+++ test/jambonz-verb-test.js
@@ -205,7 +205,8 @@ test("validate correct verbs", async (t) => {
         "hintsBoost": 10,
         "fastRecognitionTimeout": 2000,
         "deepgramOptions": {
-          "endpointing": 500
+          "endpointing": 500,
+          "noDelay": true,
         }
       },
       "say": {
