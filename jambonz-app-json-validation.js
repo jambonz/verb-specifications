2bf7561980a10116e3fc8dbb1026589d47254c41
Dave Horton <daveh@drachtio.org>
2026-03-05 11:26:51 -0500

    better handling of synonyms and shortcuts (#127)

--- jambonz-app-json-validation.js
+++ jambonz-app-json-validation.js
@@ -4,6 +4,27 @@ const _specData = require('./specs');
 const specs = new Map();
 for (const key in _specData) { specs.set(key, _specData[key]); }
 
+/* verb synonyms and shortcuts: maps alias verb names to their canonical form,
+   optionally injecting properties (e.g. vendor) into the verb data */
+const verbTransforms = new Map([
+  ['stream', {verb: 'listen'}],
+  ['s2s', {verb: 'llm'}],
+  ['openai_s2s', {verb: 'llm', properties: {vendor: 'openai'}}],
+  ['microsoft_s2s', {verb: 'llm', properties: {vendor: 'microsoft'}}],
+  ['google_s2s', {verb: 'llm', properties: {vendor: 'google'}}],
+  ['elevenlabs_s2s', {verb: 'llm', properties: {vendor: 'elevenlabs'}}],
+  ['deepgram_s2s', {verb: 'llm', properties: {vendor: 'deepgram'}}],
+  ['voiceagent_s2s', {verb: 'llm', properties: {vendor: 'voiceagent'}}],
+  ['ultravox_s2s', {verb: 'llm', properties: {vendor: 'ultravox'}}],
+]);
+
+function applyVerbTransform(name, data) {
+  const transform = verbTransforms.get(name);
+  if (!transform) return {name, data};
+  const newData = transform.properties ? {...transform.properties, ...data} : data;
+  return {name: transform.verb, data: newData};
+}
+
 function normalizeJambones(logger, obj) {
   if (!Array.isArray(obj)) {
     throw new Error('malformed jambonz payload: must be array');
@@ -13,18 +34,22 @@ function normalizeJambones(logger, obj) {
     if (typeof tdata !== 'object') throw new Error('malformed jambonz payload: must be array of objects');
     if ('verb' in tdata) {
       // {verb: 'say', text: 'foo..bar'..}
-      const name = tdata.verb;
       const o = {};
       Object.keys(tdata)
         .filter((k) => k !== 'verb')
         .forEach((k) => o[k] = tdata[k]);
+      const {name, data} = applyVerbTransform(tdata.verb, o);
       const o2 = {};
-      o2[name] = o;
+      o2[name] = data;
       document.push(o2);
     }
     else if (Object.keys(tdata).length === 1) {
       // {'say': {..}}
-      document.push(tdata);
+      const key = Object.keys(tdata)[0];
+      const {name, data} = applyVerbTransform(key, tdata[key]);
+      const o2 = {};
+      o2[name] = data;
+      document.push(o2);
     }
     else {
       logger.info(tdata, 'malformed jambonz payload: missing verb property');
